fc0c37f3dc83bc4a50e5872bea8785f7fa978591
Hitallo Cavalcanti <hcs3@cin.ufpe.br>
2023-03-19 16:08:01 +0000

    Initial commit

---
+++ src/test/java/br/api/v1/brpeopleservice/BrPeopleServiceTest.java
@@ -0,0 +1,21 @@
+package br.api.v1.brpeopleservice;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class BrPeopleServiceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
